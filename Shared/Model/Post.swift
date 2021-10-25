edecaad42197c8a43e37074209e232919d5e9414
sikandarali307 <sikandar.ali307@gmail.com>
2021-10-25 16:25:52 +0500

    posting a model

---
+++ Shared/Model/Post.swift
@@ -0,0 +1,13 @@
+//
+//  Post.swift
+//  DoubleSidedGallery (iOS)
+//
+//  Created by Mac on 25/10/2021.
+//
+
+import SwiftUI
+
+struct Post: Identifiable {
+    var id = UUID().uuidString
+    var postImge :String
+}
