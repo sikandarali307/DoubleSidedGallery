776668e238ea774b70a5fb2281f2d9a52bfa4d6f
sikandarali307 <sikandar.ali307@gmail.com>
2021-10-25 15:35:56 +0500

    Initial Commit

---
+++ Shared/DoubleSidedGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoubleSidedGalleryApp.swift
+//  Shared
+//
+//  Created by Mac on 25/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct DoubleSidedGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
